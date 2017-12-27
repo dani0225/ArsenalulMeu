1dd4f281801110204a42913b4ab77c15cd29bd17
Admin <admin@admins-macbook-pro-2.local>
2017-12-27 18:27:46 +0200

    am adult cum sa modifici un outlet sau action

---
+++ ArsenalulMeu/modificare nume Outlet sau Action.swift
@@ -0,0 +1,12 @@
+//
+//  modificare nume Outlet sau Action.swift
+//  ArsenalulMeu
+//
+//  Created by Admin on 27/12/2017.
+//  Copyright © 2017 home. All rights reserved.
+//
+
+import Foundation
+
+// daca vrem sa modificam numele unui Outlet conectat deja la UI
+//folosim commmand click si apoi rename, si o sa ii da rename si la outlet si la conexiunea dintre cod si UI
